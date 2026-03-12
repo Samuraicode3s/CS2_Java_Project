3abde8f8096aa812ec397514dbaf265cd55f4ae7
Dust <101909991+DustonCrack@users.noreply.github.com>
2026-03-11 21:40:56 -0400

    Revert "update"
    This reverts commit d46296aba79c167bf53310d2de081043562a1a25.

--- src/emailclient/Test.java
+++ src/emailclient/Test.java
@@ -3,8 +3,7 @@ package emailclient;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("This is a test from Eclipse");
-		System.out.print("HELLO");
+		System.out.print("This is a test");
 
 	}
 
