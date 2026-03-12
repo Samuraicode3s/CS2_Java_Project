550e9820f6d4009a43141497c9ce2e0be660acf4
Dust <101909991+DustonCrack@users.noreply.github.com>
2026-03-11 21:40:44 -0400

    Revert "Merge branch 'main' of git@github.com:Samuraicode3s/CS2_Java_Project.git into main"
    This reverts commit 58395d36132962d75b25b11f6efb558d2e96a6b9, reversing
    changes made to d46296aba79c167bf53310d2de081043562a1a25.

--- src/emailclient/Attachment.java
+++ src/emailclient/Attachment.java
@@ -1,42 +1,25 @@
 package emailclient;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class Attachment {
 
-    private String fileName;
-    private long fileSize;
+	private String fileName;
+	private long fileSize;
 
-    public Attachment(String fileName, long fileSize) {
-        this.fileName = fileName;
-        this.fileSize = fileSize;
-    }
+	public Attachment(String fileName, long fileSize) {
+		this.fileName = fileName;
+		this.fileSize = fileSize;
+	}
 
-    public void download() {
-        try {
-            File file = new File(fileName);
-            FileOutputStream fos = new FileOutputStream(file);
+	public void download() {
 
-            byte[] data = new byte[(int) fileSize];
-            fos.write(data);
+	}
 
-            fos.close();
+	public String getFileName() {
+		return fileName;
+	}
 
-            System.out.println("File downloaded successfully: " + fileName);
-        } 
-        catch (IOException e) {
-            System.out.println("Error downloading file: " + e.getMessage());
-        }
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public long getFileSize() {
-        return fileSize;
-    }
+	public long getFileSize() {
+		return fileSize;
+	}
 
 }
