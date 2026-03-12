d46296aba79c167bf53310d2de081043562a1a25
adama <adama@Adam.mynetworksettings.com>
2026-03-11 21:35:45 -0400

    update test 9:35

--- src/emailclient/Test.java
+++ src/emailclient/Test.java
@@ -3,7 +3,8 @@ package emailclient;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("This is a test");
+		System.out.print("This is a test from Eclipse");
+		System.out.print("HELLO");
 
 	}
 
