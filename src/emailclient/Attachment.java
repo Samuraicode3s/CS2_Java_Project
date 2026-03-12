1d21f6c92467a5db7b48f41da69983d4c385631d
Dust <101909991+DustonCrack@users.noreply.github.com>
2026-03-11 21:30:45 -0400

    ..

--- src/emailclient/Attachment.java
+++ src/emailclient/Attachment.java
@@ -1,25 +1,42 @@
 package emailclient;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Attachment {
 
-	private String fileName;
-	private long fileSize;
+    private String fileName;
+    private long fileSize;
 
-	public Attachment(String fileName, long fileSize) {
-		this.fileName = fileName;
-		this.fileSize = fileSize;
-	}
+    public Attachment(String fileName, long fileSize) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+    }
 
-	public void download() {
+    public void download() {
+        try {
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
 
-	}
+            byte[] data = new byte[(int) fileSize];
+            fos.write(data);
 
-	public String getFileName() {
-		return fileName;
-	}
+            fos.close();
 
-	public long getFileSize() {
-		return fileSize;
-	}
+            System.out.println("File downloaded successfully: " + fileName);
+        } 
+        catch (IOException e) {
+            System.out.println("Error downloading file: " + e.getMessage());
+        }
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
 
 }
